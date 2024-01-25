61b52d4cecdc074dc48dff51d42692b3492df217
Timmy <timmy.the.nobody@gmail.com>
2024-01-25 13:19:16 +0100

    chore: remove sample file
    The documentation has way better examples now

--- Shared/Index.lua
+++ Shared/Index.lua
@@ -2,8 +2,6 @@ Package.Require("classlib.lua")
 Package.Require("baseclass.lua")
 Package.Require("baseclassex.lua")
 
--- Package.Require("sample.lua")
-
 Package.Export("ClassLib", ClassLib)
 Package.Export("BaseClass", BaseClass)
 Package.Export("BaseClassEx", BaseClassEx)
